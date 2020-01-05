829a51d48e2476579105243de60d9e27ba131ed1
SongYintao <songyintao92@163.com>
2020-01-05 22:05:36 +0800

    update

--- pkg/creational/builder/builder_test.go
+++ pkg/creational/builder/builder_test.go
@@ -1,11 +1,8 @@
 package builder
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestSmallCar_Drive(t *testing.T) {
-
 	type fields struct {
 		Color Color
 		Speed Speed
@@ -18,7 +15,6 @@ func TestSmallCar_Drive(t *testing.T) {
 	}{
 		// TODO: Add test cases.
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			sc := &SmallCar{
